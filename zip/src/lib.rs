988f813b15692400333d10f4431555f7b014ee5b
Frondorf, Niklas <niklas@frondorf.eu>
2025-10-12 16:06:17 +0200

    fix(zip): fixed the encryption bit flag to use the 0th bit (which indicates encryption)

--- zip/src/lib.rs
+++ zip/src/lib.rs
@@ -8,11 +8,11 @@ pub fn try_remove_encryption_bits(data: &[u8]) -> Result<Vec<u8>> {
     let mut ziparchive = ZipArchive::try_from(data)?;
 
     for zipfile in ziparchive.zip_files.iter_mut() {
-        zipfile.local_file_header.general_purpose &= !(1 << 3);
+        zipfile.local_file_header.general_purpose &= !1;
     }
 
     for cdh in ziparchive.central_directory_headers.iter_mut() {
-        cdh.general_purpose &= !(1 << 3);
+        cdh.general_purpose &= !1;
     }
 
     Ok(ziparchive.to_bytes())
